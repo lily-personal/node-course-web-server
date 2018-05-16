edc6aae17ef16ab56b7f83b7653564faf9fb7f57
Lillian Li <lfli@Lillians-MacBook-Pro.local>
2018-05-15 17:19:43 -0700

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express=require('express');
 const hbs=require('hbs');
+const fs=require('fs');
 
+const port=process.env.PORT || 3000;
 var app=express();
 
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine','hbs');
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+  var log=`${now}:${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log+'\n');
+  next();
+})
 app.use(express.static(__dirname+'/public'));
 
-app.use((req,res,next)=>{
-  res.render('Maintenance.hbs');
-})
+//app.use((req,res,next)=>{
+//  res.render('Maintenance.hbs');
+//})
+
 app.get('/',(req,res)=>{
   res.render('home.hbs',{
     pageTitle:'Home Page',
@@ -32,6 +42,6 @@ app.get('/Bad',(req,res)=>{
   });
 });
 
-app.listen(3000,()=>{
-  console.log('The server is listening on port 3000');
+app.listen(port,()=>{
+  console.log(`The server is listening on port ${port}`);
 });
